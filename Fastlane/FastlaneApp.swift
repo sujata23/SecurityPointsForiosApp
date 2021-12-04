04fa3c53db8d3f0597ddcc7865fe89be7a0a809c
Sujata <sujata.chakraborty@nentgroup.com>
2021-12-04 21:51:13 +0100

    first commit

---
+++ Fastlane/FastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneApp.swift
+//  Fastlane
+//
+//  Created by Sujata Chakraborty on 2021-12-04.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FastlaneUITests/FastlaneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FastlaneUITestsLaunchTests.swift
+//  FastlaneUITests
+//
+//  Created by Sujata Chakraborty on 2021-12-04.
+//
+
+import XCTest
+
+class FastlaneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
